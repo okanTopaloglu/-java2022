3941d3a2b2703b83367a7eca8bfa76f89fc7e68a
PC <PC@Okan>
2022-09-25 18:14:29 +0300

    conditionals

---
+++ conditionals/src/conditionals/conditionals.java
@@ -0,0 +1,23 @@
+package conditionals;
+
+public class conditionals 
+{
+
+	public static void main(String[] args) 
+		{
+		int sayi = 24 ;
+		if (sayi<25) 
+			{ 
+				System.out.println("sayı 25 den küçüktür.");
+			}
+		
+		else if(sayi>25){
+			System.out.println("satı 25 den büyüktür.");
+		}
+		else {
+			System.out.println("sayı 25 tir");
+		}
+		}
+	
+
+}
