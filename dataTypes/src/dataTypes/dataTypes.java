d07f7f4b38d01734868d9f3da4b75afffe2e5100
PC <PC@Okan>
2022-09-25 17:18:42 +0300

    dataTypes

---
+++ dataTypes/src/dataTypes/dataTypes.java
@@ -0,0 +1,12 @@
+package dataTypes;
+
+public class dataTypes {
+	int sayi = 15 ;
+	String sehir = "Ankara" ;
+	char karakter = 'A' ;
+	double ondalik = 12.3356 ;
+	boolean dogruMU = false ;
+	boolean dogru = true ;
+	
+
+}
