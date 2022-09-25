c20135683bcf42b239a1672274f43e6f445979b8
PC <PC@Okan>
2022-09-25 18:27:05 +0300

    En buyuk sayi

---
+++ ReCap1_EnBuyukSayi/src/ReCap1_EnBuyukSayi.java
@@ -0,0 +1,22 @@
+
+public class ReCap1_EnBuyukSayi {
+
+	public static void main(String[] args) {
+		int sayi1 = 49;
+		int sayi2 = 5 ;
+		int sayi3 = 3 ;
+		
+		if(sayi1>sayi2) {
+			if(sayi1>sayi3) {
+				System.out.println("en büyük sayi" + sayi1);
+			}}
+			else if(sayi2>sayi3) {
+				System.out.println("en büyük sayi" + sayi2);
+				}
+			else { System.out.println("En büyük sayı" + sayi3);
+		}
+			
+
+	}
+
+}
