4ea1eb35e944aa8cbf7336310f91775a42320cad
PC <PC@Okan>
2022-09-26 21:07:09 +0300

    Sesli sessiz harfler

---
+++ SesliSessizHarfler/src/SesliSessizHarfler.java
@@ -0,0 +1,40 @@
+
+public class SesliSessizHarfler {
+
+	public static void main(String[] args) {
+		char harf = 'O';
+		switch(harf) {
+		case 'A': 
+			System.out.println("Sesli Harf");
+			break;
+		case 'E': 
+			System.out.println("Sesli Harf");
+			break;
+		case 'I': 
+			System.out.println("Sesli Harf");
+			break;
+		case 'İ': 
+			System.out.println("Sesli Harf");
+			break;
+		case 'O': 
+			System.out.println("Sesli Harf");
+			break;
+		case 'Ö': 
+			System.out.println("Sesli Harf");
+			break;
+		case 'U': 
+			System.out.println("Sesli Harf");
+			break;
+		case 'Ü': 
+			System.out.println("Sesli Harf");
+			break;
+			default :
+				System.out.println("Sessiz Harf");
+				break;
+				
+			
+		}
+
+	}
+
+}
