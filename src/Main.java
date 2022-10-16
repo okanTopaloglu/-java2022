8de44cf9530c8b4b7969d33886e1f661d8c7e8f3
PC <PC@Okan>
2022-10-16 16:55:54 +0300

    DortIslem

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(3, 4);
+		System.out.println(sonuc);
+		int sonuc2 =dortIslem.Cıkar(5, 1);
+		System.out.println(sonuc2);
+		int sonuc3 = dortIslem.Carp(5, 2);
+		System.out.println(sonuc3);
+		int sonuc4 = dortIslem.Bol(18, 6);
+		System.out.println(sonuc4);
+		
+	}
+
+}
