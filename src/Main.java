3e4a5621dafd69a97ba5c13876c464d56df5764c
Okan Topaloğlu <64662793+okanTopaloglu@users.noreply.github.com>
2022-10-16 16:59:18 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		DortIslem dortIslem = new DortIslem();
-		int sonuc = dortIslem.Topla(3, 4);
-		System.out.println(sonuc);
-		int sonuc2 =dortIslem.Cıkar(5, 1);
-		System.out.println(sonuc2);
-		int sonuc3 = dortIslem.Carp(5, 2);
-		System.out.println(sonuc3);
-		int sonuc4 = dortIslem.Bol(18, 6);
-		System.out.println(sonuc4);
+		
+		System.out.println("Hello World");
 		
 	}
 
